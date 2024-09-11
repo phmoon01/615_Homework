b0f83f6b1a58fd8b77b5e7952230cd6abee04f45
Paul Moon <phmoon2001@bu.edu>
2024-09-11 17:22:26 -0400

    9/11/2024 5:22pm

---
+++ HW2.R
@@ -0,0 +1,32 @@
+### YOUR NAME HERE ###
+
+### Homework 2                                         ###
+### GGPlot Basics ###
+
+#Put your code in this file. Make sure you assign the relevant values to the correct variable names, which are given below. 
+#Uncomment the variables as you assign your final values/functions/results to them.
+
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(ggplot2)
+library(stringr)# This loads the packages necessary to run your plots. Do not delete or comment this out.
+
+### Exercise 1
+
+spx_plot1<-
+spx_plot2<-
+
+### Exercise 2
+  
+bookplot<-
+  
+### Exercise 3
+  
+squirrelPlot<-
+  
+### Exercise 4
+  
+bigMac<-
+logBigMac<-
+  
